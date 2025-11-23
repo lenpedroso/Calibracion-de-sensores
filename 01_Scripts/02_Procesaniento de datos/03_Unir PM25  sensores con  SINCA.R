6e19ec08f19de44c3639994de1e2ab7f13dbb613
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-23 17:09:43 -0300

    Modificacion de codigos de fecha

--- 01_Scripts/02_Procesaniento de datos/03_Unir PM25  sensores con  SINCA.R
+++ 01_Scripts/02_Procesaniento de datos/03_Unir PM25  sensores con  SINCA.R
@@ -2,48 +2,59 @@
 # de bajo costo instalados en Santiago de Chile, especificamente en la comuna Las Condes
 # Se tomo de base el repositorio de la profesora Sara Acevedo para modificar datos
 # temporales (https://github.com/Saryace/datascience-sinca/blob/main/codigo/02_procesamiento-horas.R)
-# Se empleo chat gpt para ordenar, modificar, transformar,  optimizar codigos y unir codigos
+# y para usar la funcion joing
+# Para transformar fechas se uso la siguiente página web (https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/as.POSIX*)
+# y (https://certidevs.com/tutorial-r-lubridate-fechas-y-tiempo-hora)
+# Para convertir datos a numerico se uso el siguiente link (https://r-coder.com/funcion-as-numeric-r/)
+#Se empleo chat gpt para ordenar, modificar, transformar,  optimizar codigos y unir codigos
 # Nota: Para usar la libreria open air es necesario tener una columana que se llame date
 
-# Cargar librerias --------------------------------------------------------
+# 1. Cargar librerias --------------------------------------------------------
 
 library(dplyr)
 library(lubridate)
 
-# Modificar formato de la fecha del SINCA a formato (fecha-hora POSIXct)-------------------------------------------
+# 2. Modificar formato de la fecha del SINCA a formato (fecha-hora POSIXct)-------------------------------------------
 
-Las_Condes_Sinca_date <- Las_Condes_Sinca %>%
-  mutate(date_hour = dmy_hm(date, tz = "America/Santiago")) %>% # Convertir date en formato POSIXct y zona horaria Santiago
-  transmute(date_hour, PM25 = as.numeric(PM25)) # Devuelve dos columanas date_hour y PM25(numerico) y descarta las demás
+#2.1. Crear un nuevo df con las fechas corregidas
+Las_Condes_Sinca_date <- Las_Condes_Sinca
 
-# Modificar formato de la fecha Sensores -------------------------------------------
+#2.2. Seleccionar la columna que voy a convertir a formato POSIXct y zona horaria Santiago
+Las_Condes_Sinca_date$date_hour <- as.POSIXct(
+  Las_Condes_Sinca_date$date,
+  format = "%d/%m/%Y %H:%M",
+  tz =  "Etc/GMT+4"
+)
+# 2.3 Convertir la concentracion de PM25 a numerico
+Las_Condes_Sinca_date$PM25 <- as.numeric(Las_Condes_Sinca_date$PM25)
+  
+# 2.4 Seleccionar para el nuevo df solo las columnas necesarias (PM25 y date_hour)
+Las_Condes_Sinca_date <- Las_Condes_Sinca_date[, c("date_hour", "PM25")]
 
-Condes_Plantower <- Condes_Plantower %>%
-  mutate(
-    Fecha = Fecha + 20000000L,# Es necesario sumarle 2000 a la fecha 
-    Hora_chr = sprintf("%02d:00", Hora %/% 100),# Divide entre 100 y fijas los minutos en 00
-    date_hour = as.POSIXct(    # Especifica el tipo de formato de hora (as.POSIXct)
-      paste(Fecha, Hora_chr),  # Para unir ambas partes (año + hora)
-      format = "%Y%m%d %H:%M",  # Especifica el formato: año-mes-dia-hora-minuto
-      tz = "Etc/GMT+4"          # Define la zona horaria de Chile
-    )
-  )
-# # Unir las concentraciones del SINCA al df Condes_Plantawer por fecha ---------------------
+# 3. Modificar formato de la fecha Sensores -------------------------------------------
+
+Condes_Plantower$Fecha<-Condes_Plantower$Fecha+ 20000000
+Condes_Plantower$Hora<-Condes_Plantower$Hora/100
+Condes_Plantower$Hora<-paste(Condes_Plantower$Hora,"00",sep=":",collapse = NULL)
+Condes_Plantower$date_hour<-paste(Condes_Plantower$Fecha, Condes_Plantower$Hora,sep = " ", collapse = NULL)
+Condes_Plantower$date_hour <- as.POSIXct(Condes_Plantower$date_hour, format = "%Y%m%d %H:%M", tz = "Etc/GMT+4")
+
+# 4. Unir las concentraciones del SINCA al df Condes_Plantawer por fecha ---------------------
 
 Condes_Plantower_1 <- Condes_Plantower %>%
   left_join(Las_Condes_Sinca_date %>% select(date_hour, PM25_SINCA = PM25),
             by = "date_hour")  
 
-# # Crear un nuevo df con las concentraciones de PM25 de Plantawer y SINCA --------
+# 5. Crear un nuevo df con las concentraciones de PM25 de Plantawer y SINCA --------
 
 Condes_Plantower_SINCA <- Condes_Plantower_1 %>%
   select(date_hour, PM25_P1:PM25_P5, PM25_SINCA)
 
-# Modificar el nombre date_hour y añade columna temporales ----------------
+# 6. Modificar el nombre date_hour y añade columna temporales ----------------
 
 Condes_Plantower_SINCA <- Condes_Plantower_SINCA %>%
   rename(date = date_hour) %>% # Modificar el nombre date_hour por date
   mutate(                   # Añadir dos columnas temporales date y wday
     hour = hour(date),
-    wday = wday(date, label = TRUE, abbr = TRUE, week_start = 1) 
+    wday = wday(date, label = TRUE, week_start = 1) 
   )
