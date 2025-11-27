4ac342629f50a7065a3c7fca2b43b45f1ccedc6b
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 20:42:06 -0300

    ordenar codigos

--- 01_Scripts/01_Cargar_datos/01_Cargar_datos_sensores_(Plantower).R.R
+++ 01_Scripts/01_Cargar_datos/01_Cargar_datos_sensores_(Plantower).R.R
@@ -1,10 +1,10 @@
-# Este script permite descargar los datos de concentracion de los Plantower (sensores de bajo costos) 
+# Este script permite descargar los datos de concentración de los Plantower (sensores de bajo costos) 
 # Es necesario instalar paquete para leer csv (install.packages("readr").  
 # Los datos se cargan desde mi cuenta de git hub personal
 # Se emplearon las siguientas páginas web como base para delimitar las columnas
 #(https://www.rdocumentation.org/packages/readr/versions/2.1.6/topics/read_delim) y 
 # (https://readr.tidyverse.org/reference/read_delim.html)
-# Chat gptfue empleado para ordenar codigos y corregir errores
+# Chat gpt fue empleado para ordenar codigos y corregir errores
 
 # 1. Cargar libreria ---------------------------------------------------------
 library(readr)
@@ -16,5 +16,6 @@ Condes_Plantower<- read_csv(url)
 
 # 3. Especificar delimitador -------------------------------------------------
 
-Condes_Plantower <- read_delim(url, delim = ";", locale = locale(decimal_mark = ","))
+Condes_Plantower <- read_delim(url, delim = ";", locale = locale
+                               (decimal_mark = ","))
 
