3837bb2aee6dbeaaac6cdc385ffdfc2228322856
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 21:10:17 -0300

    Modificar texto y arreglar codigo

--- 01_Scripts/02_Procesaniento_de_datos/04_Modificar_estructura_de_los_datos.R
+++ 01_Scripts/02_Procesaniento_de_datos/04_Modificar_estructura_de_los_datos.R
@@ -1,13 +1,18 @@
-# Chat GPT fue utilizado para ordenar el codigo
-# Se tomo de base el repositorio de la profesora Sara Acevedo para transformar
-# los datos a diferentes formatos (https://github.com/Saryace/environ-dataviz-uc/tree/main/scr/05-semana)
+# Permite modificar la estructura de los datos a formato long.
+# Se tomó de base el repositorio de la profesora Sara Acevedo para transformar
+# los datos a diferentes formatos (https://github.com/Saryace/environ-dataviz-uc/tree/main/scr/05-semana)  
+# Para el uso de la funcion ifself (automatiza para cada observación)
+# (https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/ifelse)
+# Chat GPT fue utilizado para ordenar, modificar y corregir errores de código. 
+# Chat GPT fue utilizado para para identificar y consultar las funciones 
+# requeridas en el desarrollo del trabajo.
 
-# # Cargar librerias ------------------------------------------------------
+# 1. Cargar librerías ------------------------------------------------------
 
 library(tidyverse)
 library(ggplot2)
 
-# # Transformar a formato largo -------------------------------------------
+# 2.Transformar a formato largo -------------------------------------------
 
 Condes_Plantower_SINCA_long<-Condes_Plantower_SINCA%>% 
   pivot_longer(cols = c(
@@ -16,20 +21,22 @@ Condes_Plantower_SINCA_long<-Condes_Plantower_SINCA%>%
     names_sep = "_",
     values_to = "value")
 
-# Modificar para realizar un analisis de regresion
-Condes_Plantower_SINCA_long_2<-Condes_Plantower_SINCA_long %>% pivot_longer(
+# 3. Modificar para realizar un análisis de regresión---------------------
+Condes_Plantower_SINCA_long_2 <- Condes_Plantower_SINCA_long %>% pivot_longer(
   cols = c(PM25_SINCA),
   names_to  = c("variable_SINCA", "sensor_SINCA"),
   names_sep = "_",
   values_to = "value_SINCA"
 )
-# # Hacer una columna long para agregar los datos SINCA en la categoría sensores (Analisis temporal) --------
-
+# 4. Hacer una columna long para agregar los datos SINCA en la categoría sensores (grafico de variación temporal) --------
 Condes_Plantower_SINCA_long3 <- Condes_Plantower_SINCA_long %>%
-  select(wday, hour, sensor, value, PM25_SINCA) %>%
   pivot_longer(
     cols = c(value, PM25_SINCA),
-    names_to = "sensores",
+    names_to  = "sensores",
     values_to = "value_PM25"
   ) %>%
   mutate(sensor = ifelse(sensores == "PM25_SINCA", "SINCA", sensor))
+
+
+
+
