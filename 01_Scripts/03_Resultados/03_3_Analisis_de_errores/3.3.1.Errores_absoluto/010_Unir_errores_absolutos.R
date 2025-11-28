efc8979b18ebcc84cef6b7aec73adc31a59eefac
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 22:22:56 -0300

    cambio texto

--- 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.1.Errores_absoluto/010_Unir_errores_absolutos.R
+++ 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.1.Errores_absoluto/010_Unir_errores_absolutos.R
@@ -2,7 +2,7 @@
 # Se empleo para unir los dos df como base la informacion del siguiente link
 # (https://dplyr.tidyverse.org/reference/bind_rows.html)
 # Chat gpt fue empleado para modificar, ordenar y corregir errores en el codigo
-# Empleo rbind porque me garantiza quue los datos se mantengas a lo largo 
+# Empleo rbind porque me garantiza quue los datos (listas) se mantengas a lo largo 
 # uniendo solo por filas para despues poder usar ggplot sin aplicarle mayor 
 #transformación a los datos
 
@@ -13,7 +13,7 @@ library(dplyr)
 # 2. Crear una tabla con los errores absolutos completos --------------------
 
 
-errores_completo <- bind_rows(    # Une df por columnas aumentando el numero de filas
+errores_completo <- bind_rows(    # Une listas por columnas aumentando el numero de filas
   errores_no_calibrados, errores_calibrados) 
 
 
