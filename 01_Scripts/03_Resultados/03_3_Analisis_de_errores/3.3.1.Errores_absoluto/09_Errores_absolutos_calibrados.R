e9df7c333a81f2a6b5a5b64ba92991bb5646e977
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 21:59:28 -0300

    Arreglo estructura

--- 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.1.Errores_absoluto/09_Errores_absolutos_calibrados.R
+++ 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.1.Errores_absoluto/09_Errores_absolutos_calibrados.R
@@ -1,15 +1,7 @@
-# Calculo de los errores absoluto para cada observación
+# Calculo de los errores absoluto para cada observación después de la calibración
 # Para realizar las funciones se empleo el siguiente link (https://github.com/Saryace/environ-dataviz-uc/blob/main/scr/04-semana/03-funciones.R) 
 # y (https://github.com/Saryace/environ-dataviz-uc/blob/main/scr/05-semana/03-funciones-ggplot.R)extraido del repositorio de la profesora 
-#Se utlizaron los diferentes link para usar map y seleccionar elementos de una lista
-#(https://purrr.tidyverse.org/reference/map_dfr.html#arguments) y 
-#(https://stackovercoder.es/programming/1169456/the-difference-between-bracket-and-double-bracket-for-accessing-the-el)
-# Para crear tabla se baso en el codigo (https://tibble.tidyverse.org/reference/tibble.html)
-# Para el modelo de regresion lineal(https://www.datacamp.com/es/tutorial/linear-regression-R)
-# El uso de form para crear bucles me base de este link (https://r-coder.com/for-en-r/)
-# El empleo de reformulate (para simplificar la formula) me base del siguiente link (https://www.r-bloggers.com/2023/06/simplifying-model-formulas-with-the-r-function-reformulate/)
-# El empleo de predict (https://www.digitalocean.com/community/tutorials/predict-function-in-r)
-# Chat gpt fue empleado para modificar, ordenar y corregir errores en el codigo
+# Chat gpt fue empleado para modificar, ordenar y corregir errores en el codigo.
 
 # 1. Cargar libreria  ------------------------------------------------------
 library(purrr)
@@ -28,7 +20,6 @@ Valores_calibrados <- Valores_calibrados %>%
 
 #  5. Ajustar modelo: PM25_SINCA vs sensor (Plantower)  ---------------------
 # 5.1 Construir formula de regresion lineal automatizada y simplificada por sensor
-
 form <- as.formula(paste("PM25_SINCA ~", sensor))
 # 5.2 Ajuste del modelo de regrsion lineal automatizado para cada sensor 
 modelo <- lm(form, data = Valores_calibrados)
