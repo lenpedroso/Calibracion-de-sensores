e0aef4d99e4db18503b31703f9c206aaac293f3a
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 23:41:19 -0300

    eliminar spacios

--- 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.2.Parametros_de_error/016_Unir_parametros_de_errores.R
+++ 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.2.Parametros_de_error/016_Unir_parametros_de_errores.R
@@ -1,17 +1,15 @@
-# Peromite unir todos los parametros de los errores (MGE, SDMGE,MB,SD-MB) en una sola tabla
-# Se baso en (https://r-coder.com/factor-en-r/) para construir el codigo
-# Se empleo bind_rows para mantener en formato largo el df y unir filas (https://r-coder.com/rbind-cbind-en-r/)
+# Permite unir todos los parametros de los errores (MGE, SDMGE,MB,SD-MB) en una sola tabla
+# Se basó en (https://r-coder.com/factor-en-r/) para construir el codigo
+# Se empleó bind_rows para mantener en formato largo la lista y unir filas (https://r-coder.com/rbind-cbind-en-r/)
 # Chat gpt fue empleado para modificar, ordenar y corregir errores en el codigo
 
 # 1. Unir los valores de los errrores antes y despues de la calibra --------
-
 Errores_antes_despues <- bind_rows(
   tabla_errores_no_calibrado %>% mutate(Clasificación = "No calibrados"),
   tabla_errores_calibrado %>% mutate(Clasificación = "Calibrados")
 )
 
 # 2. Especificar los niveles a utilizar --------------------------------------
-
 Errores_antes_despues$Clasificación <- factor(
   Errores_antes_despues$Clasificación,
   levels = c("No calibrados", "Calibrados")
