4c7cbfe5bec92a295456590916b7112176de7cf0
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 21:25:33 -0300

    modificar texto y elimar que se guarde automatico

--- 01_Scripts/03_Resultados/03_1_Tendencia_temporal/06_Grafico_variacion_temporal.R
+++ 01_Scripts/03_Resultados/03_1_Tendencia_temporal/06_Grafico_variacion_temporal.R
@@ -2,56 +2,70 @@
 # El codigo para hacer el grafico fue extraido del repositorio  de la profesora
 # Sara Acevedo para modificar datos
 # temporales (https://github.com/Saryace/datascience-sinca/blob/main/codigo/03_plot
-# La paleta de colores fue seleccionada del sigueinte link (https://r-charts.com/es/colores/)
 # Chat gpt fue empleado para modificar, ordenar y solucionar errores de codigo
-# Cargar librerias --------------------------------------------------------
+
+# 1. Cargar librerias --------------------------------------------------------
 library (ggplot2)
-# Grafico de variacion de PM25 por horas y dias de la semana
-png("C:/Semestre 2-2025/Visualizacion/Calibracion de sensores/Calibracion de sensores/Sensores1.1/04_Figuras/tendencia.png",
-    width = 16, 
-    height = 10,
-    units = "in",
-    res = 900)
-ggplot(hour_wday, aes(x = hour, y = mean, color = sensor, fill = sensor, group = sensor)) +
-  geom_ribbon(aes(ymin = ymin, ymax = ymax), alpha = 0.15, colour = NA) +
+
+# 2. Grafico de variacion temporal por horas yu dias de la semnana -----------
+
+
+ggplot(hour_wday,
+       aes(
+         x = hour,
+         y = mean,
+         color = sensor,
+         fill  = sensor,
+         group = sensor
+       )) +
+  geom_ribbon(aes(ymin = ymin, ymax = ymax),
+              alpha = 0.15,
+              colour = NA) +
   geom_line(linewidth = 1.4) +
   facet_wrap(vars(wday), nrow = 1) +
   labs(
     x = "Hora del día",
-    y = expression(PM[2.5]~(mu*g/m^3)),
+    y = expression(PM[2.5] ~ (mu * g / m^3)),
     color = "Sensor",
     fill  = "Sensor",
     title = "Promedio horario de PM2.5 por día de la semana y sensor"
   ) +
   scale_color_manual(
     values = c(
-      "P1" = "gold",
-      "P2" = "blue",
-      "P3" = "#4EEE94",
-      "P4" = "#87CEEB",
-      "P5" = "coral",
+      "P1"   = "gold",
+      "P2"   = "blue",
+      "P3"   = "#4EEE94",
+      "P4"   = "#87CEEB",
+      "P5"   = "coral",
       "SINCA" = "#EE7AE9"
     )
   ) +
   scale_fill_manual(
     values = c(
-      "P1" = "gold",
-      "P2" =  "blue",
-      "P3" = "#4EEE94",
-      "P4" = "#87CEEB",
-      "P5" = "coral",
+      "P1"   = "gold",
+      "P2"   = "blue",
+      "P3"   = "#4EEE94",
+      "P4"   = "#87CEEB",
+      "P5"   = "coral",
       "SINCA" = "#EE7AE9"
     )
   ) +
   theme_bw() +
   theme(
-    legend.position = "right",
-    plot.title = element_text(hjust = 0.5, face = "bold"),  
-    strip.background = element_rect(fill = "grey90", color = NA),
-    strip.text = element_text(face = "bold"),
-    axis.text.x = element_text(angle = 0, hjust = 0.5)
+    legend.position   = "right",
+    plot.title        = element_text(hjust = 0.5, face = "bold"),
+    strip.background  = element_rect(fill = "grey90", color = NA),
+    strip.text        = element_text(face = "bold"),
+    axis.text.x       = element_text(angle = 0, hjust = 0.5)
   )
 
+# Obtener el grafico ------------------------------------------------------
+print(variacion)
+variacion <- png("C:/Semestre 2-2025/Visualizacion/Calibracion de sensores/Calibracion de sensores/02_Figuras/1.variacion.png",
+                 width = 16, 
+                 height = 10,
+                 units = "in",
+                 res = 900)
 
 dev.off()
 
