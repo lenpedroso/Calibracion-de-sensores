2080235d2079f51d5cc0d7785fa332ddfa9a84c8
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 21:53:56 -0300

    modificar los datos de los ejes

--- 01_Scripts/03_Resultados/03_2_Regresion_lineal/07_Scaterplot.R
+++ 01_Scripts/03_Resultados/03_2_Regresion_lineal/07_Scaterplot.R
@@ -1,10 +1,14 @@
+# Script permite optener un scaterplot para realizar un modelo de regresión lineal
 # Es necesario instalar el paquete openair para realizar el grafico
-# ## Cargar librerias -----------------------------------------------------
+
+
+#1. Cargar librerias -----------------------------------------------------
 library(openair)
 
-# Graficos de scaterplot
-  
-png("C:/Semestre 2-2025/Visualizacion/Calibracion de sensores/Calibracion de sensores/Sensores1.1/04_Figuras/scatterplot.png",
+
+# 2. Graficos de scaterplot    --------------------------------------------
+
+png("C:/Semestre 2-2025/Visualizacion/Calibracion de sensores/Calibracion de sensores/02_Figuras/2.scatterplot.png",
     width = 16, 
     height = 10,
     units = "in",
@@ -12,8 +16,8 @@ png("C:/Semestre 2-2025/Visualizacion/Calibracion de sensores/Calibracion de sen
 
 scatterPlot(
   mydata = Condes_Plantower_SINCA_long_2,
-  x      = "value_SINCA",
-  y      = "value",
+  x      = "value",
+  y      = "value_SINCA",
   method = "hexbin",
   col    = "jet",
   linear = TRUE,
@@ -21,9 +25,9 @@ scatterPlot(
   fit    = TRUE,
   type   = "sensor",
   main   = "Plantower vs SINCA por sensor",
-  xlab   = "PM2.5 SINCA (µg/m³)",
-  ylab   = "PM2.5 Sensor Plantower (µg/m³)",
-  fontsize = 14
+  xlab   = "PM2.5 Sensor Plantower (µg/m³)",
+  ylab   = "PM2.5 SINCA (µg/m³)",
+  fontsize = 15
 )
 
 dev.off()
