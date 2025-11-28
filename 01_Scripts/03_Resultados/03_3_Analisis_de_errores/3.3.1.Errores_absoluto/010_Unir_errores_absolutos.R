eec89c3bfe208d0c0e0092139086252d6b68f5fd
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 22:16:38 -0300

    arreglar codigos para unir columnas

--- 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.1.Errores_absoluto/010_Unir_errores_absolutos.R
+++ 01_Scripts/03_Resultados/03_3_Analisis_de_errores/3.3.1.Errores_absoluto/010_Unir_errores_absolutos.R
@@ -2,15 +2,17 @@
 # Se empleo para unir los dos df como base la informacion del siguiente link
 # (https://dplyr.tidyverse.org/reference/bind_rows.html)
 # Chat gpt fue empleado para modificar, ordenar y corregir errores en el codigo
+# Empleo rbind porque me garantiza quue los datos se mantengas a lo largo 
+# uniendo solo por filas para despues poder usar ggplot
 
-# Cargar librerias ------------------------------------------------------
+# 1. Cargar librerias ------------------------------------------------------
 
 library(dplyr)
 
-# Crear una tabla con los errores absolutos completos --------------------
+# 2. Crear una tabla con los errores absolutos completos --------------------
 
-errores_completo <- bind_rows( # Une diferentes df por columnas
-  errores_no_calibrados,
-  errores_calibrados
-)
+
+errores_completo <- bind_rows(    # Une df por columnas aumentando el numero de filas
+  errores_no_calibrados, errores_calibrados) 
+
 
