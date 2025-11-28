4f0ae5d4c52f564cf26f356be86e8155d968f5f3
Lenna Pedroso Noris <lpedroson@estudiante.uc.cl>
2025-11-27 23:50:15 -0300

    borrar

--- 01_Scripts/03_Resultados/NO/main.R
+++
@@ -1,7 +0,0 @@
-
-# Librerias ---------------------------------------------------------------
-library(tidyverse)
-
-# Carga de datos ----------------------------------------------------------
-leer_datos<- list.files(path = "01_Scripts/01_Cargar datos", recursive = TRUE, full.names = TRUE)
-walk(leer_datos, source)
